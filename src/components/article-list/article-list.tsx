5c27049cf0ddd8143eac561a089258adc7972d80
Anna Chirva <anna.chirva.hr@yandex.ru>
2022-04-22 17:54:11 +0300

    ref: refactor article list to functional

---
+++ src/components/article-list/article-list.tsx
@@ -0,0 +1,49 @@
+import ArticlePreview from '../ArticlePreview';
+import ListPagination from '../ListPagination';
+import React, { FC } from 'react';
+
+type TArticle = {
+  slug: string;
+};
+
+interface IArticleList {
+  articles: TArticle[];
+  pager: () => Promise<unknown>;
+  articlesCount: number;
+  currentPage: number;
+  loading?: boolean;
+}
+
+const ArticleList: FC<IArticleList> = ({
+  articles,
+  pager,
+  articlesCount,
+  currentPage,
+  loading,
+}) => {
+  console.log('ArticleList', articles);
+  console.log('pager', pager);
+
+  if (!articles) {
+    return <div className="article-preview">Loading...</div>;
+  }
+
+  if (articles.length === 0) {
+    return <div className="article-preview">No articles are here... yet.</div>;
+  }
+  return (
+    <div>
+      {articles.map((article) => {
+        return <ArticlePreview article={article} key={article.slug} />;
+      })}
+
+      <ListPagination
+        pager={pager}
+        articlesCount={articlesCount}
+        currentPage={currentPage}
+      />
+    </div>
+  );
+};
+
+export default ArticleList;
