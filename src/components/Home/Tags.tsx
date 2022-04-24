0c3fc8216ceae6392e0d3f155f9773d22f3f7e8d
Yulia Zherebtsova <juliaz1206@yandex.ru>
2022-04-24 05:58:00 +0300

    refactor: Tags.js to TS

---
+++ src/components/Home/Tags.tsx
@@ -0,0 +1,53 @@
+import React, { Dispatch, SetStateAction } from 'react';
+import agent from '../../agent';
+import { IArticleList } from '../article-list/article-list';
+
+interface TPayload {
+  pager: (page: number) => IArticleList;
+  articles: IArticleList;
+  articlesCount: number;
+  tab: string | null;
+  tag: string;
+  currentPage: number;
+}
+
+interface ITags {
+  tags: string[];
+  onClickTag: (
+    tag: string,
+    pager: (page: number) => IArticleList,
+    payload: TPayload
+  ) => Dispatch<SetStateAction<string>>;
+}
+
+const Tags: React.FC<ITags> = ({ tags, onClickTag }) => {
+  const handleClick = (ev: React.MouseEvent, tag: string) => {
+    ev.preventDefault();
+    onClickTag(
+      tag,
+      (page: number) => agent.Articles.byTag(tag, page),
+      agent.Articles.byTag(tag)
+    );
+  };
+
+  if (tags) {
+    return (
+      <div className="tag-list">
+        {tags.map((tag: string) => {
+          return (
+            <a
+              href="/"
+              className="tag-default tag-pill"
+              key={tag}
+              onClick={(evt) => handleClick(evt, tag)}>
+              {tag}
+            </a>
+          );
+        })}
+      </div>
+    );
+  }
+  return <>Loading Tags...</>;
+};
+
+export default Tags;
