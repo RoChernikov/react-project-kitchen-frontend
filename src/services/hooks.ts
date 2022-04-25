2ba05b4b34ec698b42f6767a023430ad9fc465a9
Ro Chernikov <imrhubris@gmail.com>
2022-04-25 09:12:24 +0300

    feat: basic store & hooks have been added

---
+++ src/services/hooks.ts
@@ -0,0 +1,8 @@
+// import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+// import type { RootState, AppDispatch, AppThunk } from '../services/store';
+// //--------------------------------------------------------------------------------
+
+// export const useAppDispatch = () => useDispatch<AppDispatch & AppThunk>();
+// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const shutTheFuckUp = null; //дабы TS заткнулся, to delete
---
+++ src/services/store.ts
@@ -0,0 +1,21 @@
+// import thunk, { ThunkAction } from 'redux-thunk';
+// import { Action, ActionCreator } from 'redux';
+// import { configureStore } from '@reduxjs/toolkit';
+// import { rootReducer } from './slices';
+// //--------------------------------------------------------------------------------
+
+// export type RootState = ReturnType<typeof state.getState>;
+// export type AppThunk<TReturn = void> = ActionCreator<
+//   ThunkAction<TReturn, RootState, unknown, Action>
+// >;
+// export type AppDispatch = typeof state.dispatch;
+
+// const state = configureStore({
+//   reducer: rootReducer,
+//   middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunk),
+//   devTools: process.env.NODE_ENV !== 'production'
+// });
+
+// export default state;
+
+export const shutTheFuckUp = null; //дабы TS заткнулся, to delete
