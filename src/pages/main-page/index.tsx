05edaf1f0057698bd229bdb3468521a8ec587ca0
Ro Chernikov <imrhubris@gmail.com>
2022-05-04 18:42:39 +0300

    feat: button component has been added

--- src/pages/main-page/index.tsx
+++ src/pages/main-page/index.tsx
@@ -1,19 +1,57 @@
 import React, { FC } from 'react';
 import styles from './main-page.module.scss';
 import { useLocation, Link } from 'react-router-dom';
+import { Button } from 'components/button/button';
+import PlusIcon from 'components/icons/plus-icon';
+import MinusIcon from 'components/icons/minus-icon';
+import TrashIcon from 'components/icons/trash-icon';
 //--------------------------------------------------------------------------------
 
 const MainPage: FC = () => {
   const location = useLocation();
   return (
     <div className={styles.main}>
-      MAIN PAGE
+      <h1>UI Kit</h1>
+      <p>Тест модалки</p>
       <Link
         to={`/modal`}
         state={{ backgroundLocation: location }}
         className={styles.main__link}
-        children={'ТЕСТ МОДАЛКИ'}
+        children={'Открыть модалку'}
       />
+      <p>Кнопки</p>
+      <div
+        style={{
+          display: 'flex',
+          gap: 20,
+        }}>
+        <Button
+          type="primary"
+          color="primary"
+          icon={<PlusIcon />}
+          children="Подписаться"
+        />
+        <Button
+          type="primary"
+          color="primary"
+          icon={<MinusIcon />}
+          children="Отписаться"
+        />
+        <Button type="primary" color="primary" children="Сохранить запись" />
+        <Button color="secondary" children="Удалить запись" />
+        <Button color="secondary" children="Удалить запись" disabled />
+        <Button
+          type="secondary"
+          children="Удалить запись"
+          icon={<TrashIcon />}
+        />
+        <Button
+          type="secondary"
+          children="Удалить запись"
+          icon={<TrashIcon />}
+          disabled
+        />
+      </div>
     </div>
   );
 };
