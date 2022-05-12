376fb30c3540a017121f7fcdd1983a8cd853d417
Yulia Zherebtsova <juliaz1206@yandex.ru>
2022-05-12 04:33:22 +0300

    fix: likes on MainPage

--- src/pages/main-page/index.tsx
+++ src/pages/main-page/index.tsx
@@ -1,16 +1,22 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import ArticlePreview from '../../components/article-preview/article-preview';
 import styles from './main-page.module.scss';
-import { useAppSelector } from 'services/hooks';
+import { useAppDispatch, useAppSelector } from 'services/hooks';
 import { selectArticles } from 'services/selectors/articles';
 import PopularArticles from 'components/popular-articles/popular-articles';
 import PopularTags from 'components/popular-tags/popular-tags';
+import { getArticlesData } from 'services/slices/articles';
 
 const MainPage: FC = () => {
   const articles = useAppSelector(selectArticles);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getArticlesData());
+  }, [dispatch]);
 
   if (articles.length === 0) {
-    return <div className="article-preview">No articles are here... yet.</div>;
+    return <div className="article-preview">Тут пока нет статей...</div>;
   }
 
   return (
